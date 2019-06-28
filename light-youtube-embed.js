75aaaea9d31f081230b7115af099cfc960948cdf
Amir Sadeghian <amirsa97@gmail.com>
2019-06-28 17:37:29 -0500

    1.Added a new function for extracting Youtube embed video ID. 2.Added youtube class to demo iframes.

--- light-youtube-embed.js
+++ light-youtube-embed.js
@@ -5,12 +5,24 @@
  */
 
  // DOM is ready
- console.log('A');
  document.addEventListener("DOMContentLoaded", function() {
  	//Target all the elements with youtube class
- 	let iframes = document.getElementsByClassName('.youtube');
+ 	let iframes = document.getElementsByClassName('youtube');
  	//Check to make sure array is not empty
+ 	console.log(iframes);
  	if(iframes.length > 0){
-
+ 		for(let i=0; i<iframes.length; i++){
+ 			console.log(iframes[i]);
+ 			var currentEmbedURL = iframes[i].src;
+ 			console.log(currentEmbedURL);
+ 			console.log(extractYotubeEmbedID(currentEmbedURL));
+ 		}
  	}
  });
+
+ //Function to extract the Youtube embed ID
+ function extractYotubeEmbedID(youtubeEmbedURL){
+ 	let regexPattern = /^.*((v\/)|(\/u\/\w\/)|(embed\/))\??([^#\&\?]*).*/;
+ 	let matches = youtubeEmbedURL.match(regexPattern);
+ 	return (matches && matches[5].length === 11)? matches[5] : false;
+ }
