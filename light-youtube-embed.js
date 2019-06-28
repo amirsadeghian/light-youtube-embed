765738964713ca64867a69a7fd9d3b79ada1ffe7
Amir Sadeghian <amirsa97@gmail.com>
2019-06-28 17:16:34 -0500

    Started the main JS build

---
+++ light-youtube-embed.js
@@ -0,0 +1,16 @@
+/* Light Youtube Embed v1.0.0
+ * https://github.com/amirsadeghian/light-youtube-embed
+ * Author: Amir Sadeghian
+ * 29-06-2019
+ */
+
+ // DOM is ready
+ console.log('A');
+ document.addEventListener("DOMContentLoaded", function() {
+ 	//Target all the elements with youtube class
+ 	let iframes = document.getElementsByClassName('.youtube');
+ 	//Check to make sure array is not empty
+ 	if(iframes.length > 0){
+
+ 	}
+ });
